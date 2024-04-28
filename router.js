3c43568734b1359922c7116c8f46dfed5bb81615
SpaceAws <151076484+SpaceAws@users.noreply.github.com>
2024-04-28 15:33:52 +0100

    Delete router.js

--- router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import favorites from './routes/favoriteRoutes.js'  //importation des routes
-
-const router = express.Router()
-
-router.use('/', favorites)      //activation du router pour les fontions de la favoriteRoutes en fonction de l'url
-
-export default router       //exportation du router pour l'index
